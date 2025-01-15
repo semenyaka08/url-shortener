04bdbec1c4fb739cc87042ba9413dfae53b96975
semenyaka08 <Serh.semka@gmail.com>
2025-01-15 08:57:34 +0200

    Improve client-side URL copy functionality

--- client/src/app/features/links/shortened-link/shortened-link.component.ts
+++ client/src/app/features/links/shortened-link/shortened-link.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, inject, Inject} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -9,6 +9,7 @@ import {
 import {MatFormField} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
+import {SnackbarService} from '../../../core/services/snackbar.service';
 
 @Component({
   selector: 'app-shortened-link',
@@ -27,4 +28,12 @@ import {MatButton} from '@angular/material/button';
 })
 export class ShortenedLinkComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public shortenedLink: string) {}
+
+  snackbarService = inject(SnackbarService);
+
+  copyToClipboard(value: string): void {
+    navigator.clipboard.writeText(value).then(() => {
+      this.snackbarService.success('Link copied to clipboard!')
+    });
+  }
 }
